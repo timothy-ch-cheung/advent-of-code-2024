da94ac30777aefae8ad0cb9f7faa948cb9d5d6be
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2024-12-18 01:18:00 +0000

    Add test to debug detecting corners for day 12 part 2

--- advent-of-code-2024/src/test/java/com/cheung/timothy/day12/GardenGroupsPart2Test.java
+++ advent-of-code-2024/src/test/java/com/cheung/timothy/day12/GardenGroupsPart2Test.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class GardenGroupsPart2Test {
 
     @Test
-    void shouldCountSides() {
+    void shouldCountSidesShapeH() { // h
         Node node1 = new Node("A");
         Node node2 = new Node("A");
         Node node3 = new Node("A");
@@ -40,5 +40,57 @@ class GardenGroupsPart2Test {
 
         assertEquals(10, plotStats.getSides());
         assertEquals(6, plotStats.getArea());
+    }
+
+    @Test
+    void shouldCountSidesShapeT() { // T
+        Node node1 = new Node("A");
+        Node node2 = new Node("A");
+        Node node3 = new Node("A");
+        Node node4 = new Node("A");
+
+        node1.setRight(node2);
+        node2.setLeft(node1);
+
+        node2.setRight(node3);
+        node3.setLeft(node2);
+
+        node2.setDown(node4);
+        node4.setTop(node2);
+
+        PlotStats plotStats = new PlotStats();
+        Set<Set<Node>> knownInnerCorners = new HashSet<>();
+        calculateCost(node1, plotStats, knownInnerCorners);
+
+        assertEquals(8, plotStats.getSides());
+        assertEquals(4, plotStats.getArea());
+    }
+
+    @Test
+    void shouldCountSidesShapeC() { // C
+        Node node1 = new Node("A");
+        Node node2 = new Node("A");
+        Node node3 = new Node("A");
+        Node node4 = new Node("A");
+        Node node5 = new Node("A");
+
+        node1.setLeft(node2);
+        node2.setRight(node1);
+
+        node2.setDown(node3);
+        node3.setTop(node2);
+
+        node3.setDown(node4);
+        node4.setTop(node3);
+
+        node5.setLeft(node4);
+        node4.setRight(node5);
+
+        PlotStats plotStats = new PlotStats();
+        Set<Set<Node>> knownInnerCorners = new HashSet<>();
+        calculateCost(node1, plotStats, knownInnerCorners);
+
+        assertEquals(8, plotStats.getSides());
+        assertEquals(5, plotStats.getArea());
     }
 }
